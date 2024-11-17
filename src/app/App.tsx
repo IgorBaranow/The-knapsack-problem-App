9c82adcaebc3b2f7cfd024c20019f0d16b1e9969
Igor Baranov <baranow.ig@gmail.com>
2024-11-17 14:57:41 +0100

    Initial commit

---
+++ src/app/App.tsx
@@ -0,0 +1,12 @@
+import { AppRouter } from "@config/routes";
+import { theme } from "@config/styles";
+import { CssBaseline, ThemeProvider } from "@mui/material";
+
+export default function App() {
+  return (
+    <ThemeProvider theme={theme}>
+      <CssBaseline />
+      <AppRouter />
+    </ThemeProvider>
+  );
+}
---
+++ src/pages/home.tsx
@@ -0,0 +1,10 @@
+import Button from "@mui/material/Button";
+
+export default function HomePage() {
+  return (
+    <div>
+      home
+      <Button variant="contained">Hello world</Button>;
+    </div>
+  );
+}
